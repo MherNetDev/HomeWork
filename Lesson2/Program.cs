64030e23cc95a08b33c73099665121d08577fbbe
MherNetDev <sng.bars@mail.ru>
2023-02-16 16:12:39 +0400

    Add project files.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DataTypes
+
+            //byte 0 -- 255
+            //sbyte -128 -- 128
+            //short -32768 -- 32767
+            //ushort 0 -- 65535
+            //int -2147483648 -- 2147483647
+            //uint 0 -- 4294967295
+            //long -9223372036854775808 -- 9223372036854775807
+            //ulong 0 -- 18446744073709551615
+
+            byte by = 1;
+            sbyte sby = 2;
+            short sh = 3;
+            ushort ush = 4;
+            int i = 5;
+            uint ui = 6;
+            long l = 7;
+            ulong ul = 8;
+
+            //Console.WriteLine(ul - l); ??????  Ошибка CS0034  Оператор "-" для операнда типа "ulong" и "long" является неоднозначным
+
+
+            Console.WriteLine(
+                "by = " + by + "\n" +
+                "sbyte =" + sby + "\n" +
+                "short  = " + sh + "\n" +
+                "ushort  = " + ush + "\n" +
+                "int  = " + i + "\n" +
+                "uint  = " + ui + "\n" +
+                "long  = " + l + "\n" +
+                "ulong  = " + ul + "\n");
+
+            Console.WriteLine(l + "/" + sby + "+" + ush + "+" + i + "-" + ui + "=" + (l / sby + ush + i - ui));
+
+
+
+
+            //Double Float Decimal
+            //float  4 bytes
+            //double 8 bytes
+            //decimal 16 bytes
+            //Single 32 bytes
+
+            double d = 4.5;
+            float f = 8.5f; //Առանց վերջում f գրելու չի լինի?
+            decimal dc = 16.5m; //Console.WriteLine(dc-d); Decimal-ից Float,Double չի լինում հանել չնայած որ decimal-ը ավելի մեծ Range ունի
+
+            Console.WriteLine((f + f).GetType()); //float + float = Single? 
+            Console.WriteLine((f * d).GetType());
+
+
+
+
+
+            //if else box
+
+            Console.WriteLine("\n\n" + "Type Number");
+            double strNum = Convert.ToDouble(Console.ReadLine());
+
+
+            if (strNum % 2 == 0)
+                Console.WriteLine("Even");
+            else if (strNum < 1)
+                Console.WriteLine("Number must be greater than 1");
+            else
+                Console.WriteLine("Odd");
+
+            Console.WriteLine("\n" + "How Old Are You?");
+            byte old = Convert.ToByte(Console.ReadLine());
+            if (old > 0 && old <= 13)
+                Console.WriteLine("You are a child");
+            else if (old > 13 && old <= 21)
+                Console.WriteLine("You are a teenager");
+            else if (old > 21 && old <= 49)
+                Console.WriteLine("You are a young man");
+            else if (old >= 50)
+                Console.WriteLine("You are a old man");
+
+
+
+
+
+            //Switch case  box
+            Console.WriteLine("Your Experiance in Programming from 1 to 4");
+            byte expYears = Convert.ToByte(Console.ReadLine());
+            switch (expYears)
+            {
+                case 1:
+                    Console.WriteLine("-_-");
+                    break;
+                case 2:
+                    Console.WriteLine("Middle Developer");
+                    Console.WriteLine("Junior ");
+                    break;
+                case 3:
+                    Console.WriteLine("Middle Developer");
+                    break;
+                case 4:
+                    Console.WriteLine("Senior Developer");
+                    break;
+
+                default:
+                    Console.WriteLine("Out of Range");
+                    break;
+            }
+
+            var ternary = i < d ? false : true;
+            Console.WriteLine("\n\n" + ternary);
+
+
+
+
+        }
+    }
+}
---
+++ Lesson3/Program.cs
@@ -0,0 +1,305 @@
+﻿namespace Lesson_3
+{
+    internal class Program
+    {
+        //151-166,
+        //211-280,
+        //281-310,
+        //333-356
+
+        static void Main(string[] args)
+        {
+            //21-36
+
+            int[] abcArr = new int[3] { 9, 16, 5 };
+            int maxVal = 0;
+            int minVal = abcArr[0];
+            int maxValueIndex = 0;
+            int minValueIndex = 0;
+            bool contestOne = false;
+
+
+            //21
+            for (int i = 0; i < abcArr.Length; i++)
+            {
+                if (maxVal < abcArr[i])
+                {
+                    maxVal = abcArr[i];
+                    maxValueIndex = i;
+                }
+            }
+            Console.WriteLine("21. - " + maxVal);
+
+
+            //22
+            for (int i = 0; i < abcArr.Length; i++)
+            {
+                if (minVal >= abcArr[i])
+                {
+                    minVal = abcArr[i];
+                    minValueIndex = i;
+                }
+            }
+            Console.WriteLine("22. - " + minVal);
+
+
+            //23
+            for (int i = 0; i < abcArr.Length; i++)
+            {
+                if (abcArr[i] == 1)
+                {
+                    contestOne = true;
+                }
+            }
+            Console.WriteLine("23. - " + contestOne);
+
+
+            //24
+            if (abcArr[0] == 2 && abcArr[1] == 2 || abcArr[0] == 2 && abcArr[2] == 2 || abcArr[1] == 2 && abcArr[2] == 2)
+            {
+                Console.WriteLine("24. - true");
+            }
+            else
+            {
+                Console.WriteLine("24. - false");
+            }
+
+
+            //25
+            if (abcArr[0] + abcArr[1] < 16)
+            {
+                Console.WriteLine("25. - y=1");
+            }
+            else
+            {
+                Console.WriteLine("25. - y=2");
+            }
+
+
+            //26
+            bool isOdd = false;
+            for (int i = 0; i < abcArr.Length; i++)
+            {
+                if (abcArr[i] % 2 == 0)
+                {
+                    Console.WriteLine("26. - 1");
+                    isOdd = true;
+                }
+            }
+            if (isOdd != true)
+            {
+                Console.WriteLine("26. - 2");
+            }
+
+
+            //27
+            if (abcArr[2] - abcArr[1] == abcArr[1] - abcArr[0])
+            {
+                Console.WriteLine("27. - true");
+            }
+            else
+            {
+                Console.WriteLine("27. - false");
+            }
+
+
+            //28
+            if (abcArr[2] / abcArr[1] == abcArr[1] / abcArr[0])
+            {
+                Console.WriteLine("28. - true");
+            }
+            else
+            {
+                Console.WriteLine("28. - false");
+            }
+
+
+            //29-30
+
+            int middlevalue = 0;
+            for (int i = 0; i < abcArr.Length - 1; i++)
+            {
+                if (i != maxValueIndex && i != minValueIndex)
+                {
+                    middlevalue = i;
+                    break;
+                }
+            }
+            Console.WriteLine(abcArr[minValueIndex] + "  " + abcArr[middlevalue] + "  " + abcArr[maxValueIndex]);
+            Console.WriteLine(abcArr[maxValueIndex] + "  " + abcArr[middlevalue] + "  " + abcArr[minValueIndex]);
+
+
+            //կամ էլ եթե Array-ը մեծ ա սենց կարանք սորտավորենք
+
+            //Console.Write("29. - ");
+            //int prop;
+            //for (int i = 0; i < abcArr.Length; i++)
+            //{
+            //    for (int j = 0; j < abcArr.Length; j++)
+            //    {
+            //        if (abcArr[i]> abcArr[j])
+            //        {
+            //            prop = abcArr[i];
+            //            abcArr[i] = abcArr[j];
+            //            abcArr[j] = prop;
+            //        }
+            //    }
+            //}
+            //for (int i = 0; i < abcArr.Length; i++)
+            //{
+            //    Console.Write(abcArr[i]+",");
+            //}
+
+            ////30
+            //Console.Write("\n"+"30. - ");
+            //prop = 0;
+            //for (int i = 0; i < abcArr.Length; i++)
+            //{
+            //    for (int j = 0; j < abcArr.Length; j++)
+            //    {
+            //        if (abcArr[i] < abcArr[j])
+            //        {
+            //            prop = abcArr[i];
+            //            abcArr[i] = abcArr[j];
+            //            abcArr[j] = prop;
+            //        }
+            //    }
+            //}
+            //for (int i = 0; i < abcArr.Length; i++)
+            //{
+            //    Console.Write(abcArr[i] + ",");
+            //}
+
+
+
+
+            //34
+            Console.Write("\n\n" + "34. ");
+            int[] abcdArr = new int[4] { 41, 59, 4, 202 };
+            int[] abcdArrСheck = new int[4];
+            bool checkSum = false;
+
+            int j = 0;
+            for (int i = 0; i < abcdArr.Length - 1; i++)
+            {
+                for (j = i + 1; j < abcdArr.Length; j++)
+                {
+                    Console.Write((abcdArr[i] + abcdArr[j]) + ",");
+                    abcdArrСheck[i] = (abcdArr[i] + abcdArr[j]);
+                }
+            };
+
+            int k = 0;
+            for (int i = 0; i < abcdArrСheck.Length - 1; i++)
+            {
+
+                for (k = i + 1; k < abcdArrСheck.Length; k++)
+                {
+                    if (abcdArrСheck[i] == abcdArrСheck[k])
+                    {
+                        checkSum = true;
+                    }
+                }
+            };
+            Console.WriteLine("\n" + checkSum);
+            checkSum = false;
+            //36
+            Console.Write("\n\n" + "36.");
+            for (int i = 0; i < abcdArr.Length - 1; i++)
+            {
+                for (j = i + 1; j < abcdArr.Length; j++)
+                {
+                    if (abcdArr[i] % 2 != 0 && abcdArr[j] % 2 != 0)
+                    {
+                        checkSum = true;
+
+                    }
+
+                }
+            };
+            Console.Write(checkSum);
+
+
+
+            //51
+            Console.Write("\n\n" + "51.");
+
+            int tripleNum = 459;
+            int tripleNum2 = tripleNum;
+            int first = 0;
+
+
+            bool t = false;
+            for (; tripleNum > tripleNum2 / 100 + 1;)
+            {
+                tripleNum = tripleNum / 10;
+                first = tripleNum;
+            }
+            if (tripleNum + tripleNum2 % 100 / 10 == tripleNum2 % 10)
+            {
+                t = true;
+            }
+            Console.Write(t);
+
+
+
+            //52
+
+            Console.Write("\n\n" + "52.");
+            t = false;
+            int second = tripleNum2 % 100 / 10;
+            int last = tripleNum2 % 10;
+
+            if (first == second || first == last || second == last)
+            {
+                t = true;
+            }
+            Console.Write(t);
+            Console.WriteLine(" " + first + " " + second + " " + last);
+
+
+            //211
+            Console.Write("\n\n" + "211. ");
+            int[] mAndPlus = new int[50];
+
+            for (int i = 0; i < mAndPlus.Length; i++)
+            {
+                Random rnd = new Random();
+                mAndPlus[i] = rnd.Next(-150, 150);
+            }
+            int sum = 0;
+            int incr = 1;
+            int howMany = 0;
+            for (int i = 0; i < mAndPlus.Length; i++)
+            {
+                if (mAndPlus[i] > 0)
+                {
+                    sum = sum + mAndPlus[i];
+                    incr = incr * mAndPlus[i];
+                    howMany++;
+                }
+            }
+            Console.Write(sum / howMany);
+
+            //212
+            Console.Write("\n\n" + "211. ");
+            Console.Write(Convert.ToInt64(Math.Sqrt(sum / howMany)));
+
+
+
+            //215
+            Console.Write("\n\n" + "215. ");
+            int sum2 = 0;
+            for (int i = 0; i < mAndPlus.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum2 = sum + mAndPlus[i];
+                }
+            }
+            Console.Write(sum2);
+
+
+        }
+    }
+}
