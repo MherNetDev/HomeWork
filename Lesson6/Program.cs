81fd2222d2b20e1e6e843687e15ad8aece0b3dfd
MherNetDev <sng.bars@mail.ru>
2023-02-23 16:26:32 +0400

    3

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -4,80 +4,81 @@
     {
         static void Main(string[] args)
         {
-            double salary = 0, tax = 0, clB = 0, stampDuty = 0, socialFee = 0;
 
-            Taxes(out salary, out tax);
-            Socialfee(in salary, ref  socialFee);
-            Stampduty(ref salary, ref stampDuty);
-            CleanBalance(ref salary,ref socialFee,ref stampDuty,ref clB,ref tax);
+            double wage = 0, tax = 0, clean = 0, stampDuty = 0, socialFee = 0;
+            Console.WriteLine("Type 1 For Wage to Clean   Type 2 For Clean to Wage");
+
+            Console.Clear();
+            Console.WriteLine("Type the Wage Salary");
+
+            Taxes(out wage, out tax);
+            Socialfee(in wage, ref socialFee, ref clean);
+            Stampduty(ref wage, ref stampDuty, ref clean);
+            CleanBalance(ref wage, ref socialFee, ref stampDuty, ref clean, ref tax);
 
             Console.WriteLine("Taxes          " + (int)tax);
             Console.WriteLine("Social Fee     " + (int)socialFee);
             Console.WriteLine("Stamp Duty     " + (int)stampDuty);
-            Console.WriteLine("Wage Balance   " + (int)salary);
-            Console.WriteLine("Clean Balance  " + (int)clB);
-
+            Console.WriteLine("Wage Balance   " + (int)wage);
+            Console.WriteLine("Clean Balance  " + (int)clean);
         }
 
-        public static double CleanBalance(ref double salary, ref double socialFee, ref double stampDuty, ref double clB, ref double tax)
+        public static double CleanBalance(ref double wage, ref double socialFee, ref double stampDuty, ref double clean, ref double tax)
         {
-            clB = salary - socialFee - stampDuty - tax;
-
-            return clB;
+            clean = wage - socialFee - stampDuty - tax;
+            return clean;
         }
 
-        public static double Stampduty(ref double salary, ref double stampDuty)
+        public static double Stampduty(ref double wage, ref double stampDuty, ref double clean)
         {
-            if (salary <= 100000)
+            if (wage <= 100000)
                 stampDuty = 1500;
-            else if (salary > 100000 && salary <= 200000)
+            else if (wage > 100000 && wage <= 200000)
                 stampDuty = 3000;
-            else if (salary > 200000 && salary <= 500000)
+            else if (wage > 200000 && wage <= 500000)
                 stampDuty = 5500;
-            else if (salary > 500000 && salary <= 1000000)
+            else if (wage > 500000 && wage <= 1000000)
                 stampDuty = 8500;
-            else if (salary > 1000000 )
+            else if (wage > 1000000)
                 stampDuty = 15000;
 
             return stampDuty;
         }
 
-        public static double Socialfee(in double salary,ref double socialFee)
+        public static double Socialfee(in double wage, ref double socialFee, ref double clean)
         {
-            if (salary <= 500000)
-                socialFee = salary/100*5;
-            else if (salary > 500000 && salary <= 1125000)
-                socialFee = (salary / 100 * 10)-25000;
-            else if (salary > 1125000 )
+            if (wage <= 500000)
+                socialFee = wage / 100 * 5;
+            else if (wage > 500000 && wage <= 1125000)
+                socialFee = (wage / 100 * 10) - 25000;
+            else if (wage > 1125000)
                 socialFee = 87500;
-        
+
             return socialFee;
         }
 
-        public static double Taxes(out double salary, out double tax)
+        public static double Taxes(out double wage, out double tax)
         {
-            if (double.TryParse(Console.ReadLine(), out salary)  ) {
-                if (salary < 2000)
+            if (double.TryParse(Console.ReadLine(), out wage))
+            {
+                if (wage < 2000)
                 {
                     Console.Clear();
                     Console.WriteLine("ERROR:Number must be greater/equal to 2000");
-                    Taxes(out salary, out tax);
-
+                    Taxes(out wage, out tax);
                 }
                 Console.Clear();
-
             }
             else
             {
                 Console.Clear();
                 Console.WriteLine("ERROR:Not a number");
-                Taxes(out salary, out tax);
+                Taxes(out wage, out tax);
             }
 
-            tax = salary / 100 * 20;
+            tax = wage / 100 * 20;
             return tax;
+
         }
-
-
     }
 }
